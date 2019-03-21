8f418f2370c13c8acb961f1bb69ae406edd9d424
Kevin Simper <kevin.simper@gmail.com>
2019-03-21 14:31:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Kubernetes with HTTPS 21 Marts");
+  res.send("Hello Kubernetes with HTTPS 21 Marts - Awesome");
 });
 
 const PORT = process.env.PORT || 9000;
